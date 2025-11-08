333521e21afb3de2c8ac9fb8cb1fb25122309b0f
kavya1280 <kacrylic02@gmail.com>
2025-11-08 14:39:06 +0530

    altered color and charts

--- JS/index.js
+++ JS/index.js
@@ -773,9 +773,9 @@ root.setThemes([
 ]);
 
 // Define custom colors (Hex codes verified)
-var colorGreen = am5.color("#00A878");
-var colorBlue = am5.color("#3B82F6"); 
-var colorOrange = am5.color("#F59E0B");
+var colorGreen = am5.color(0x54f88d);
+var colorBlue = am5.color(0xbef467); 
+var colorOrange = am5.color(0xF59E0B);
 
 // Create wrapper container
 var container = root.container.children.push(
@@ -906,8 +906,8 @@ series.rectangles.template.adapters.add("fill", function(fill, target) {
     var level = dataItem.get("level");
     
     // Find the Level 1 parent (the root section: INCOME, EXPENSES, PROFIT/LOSS)
-    var parentDataItem = dataItem.parent;
-    console.log({parentDataItem});  
+    var parentDataItem = dataItem.get("parent");
+    // console.log({parentDataItem});  
     while (parentDataItem && parentDataItem.get("level") > 1) {
         parentDataItem = parentDataItem.parent;
     }
@@ -915,17 +915,17 @@ series.rectangles.template.adapters.add("fill", function(fill, target) {
     if (parentDataItem) {
         console.log({parentDataItem});
         var category = parentDataItem.dataContext.name;
+        console.log({category,colors});
+        console.log("parentDataItem",parentDataItem.dataContext);
         
         if (colors[category]) {
             var colorSet = colors[category];
-            console.log("colorSet",colorSet);
+            console.log({colorSet,level});
             // For the main sections (Level 1), apply the solid base color
-            if (level === 1) {
-                if (category === "INCOME") return colorGreen;
-                if (category === "EXPENSES") return colorBlue;
-                if (category === "PROFIT / LOSS") return colorOrange;
-            }
-            
+            if (category === "INCOME") return colorGreen;
+            if (category === "EXPENSES") return colorBlue;
+            if (category === "PROFIT / LOSS") return colorOrange;
+
             // For children (Level > 1), use the color set to iterate through shades.
             return colorSet.next().color;
         }
@@ -1160,6 +1160,538 @@ series.appear(1000, 100);
 
 }); // end am5.ready()
 
+// =======================================================
+// ====Bubble chart ======================================
+// =======================================================
+am5.ready(function() {
+
+// Create root element
+// https://www.amcharts.com/docs/v5/getting-started/#Root_element
+var root = am5.Root.new("bubblechart");
+
+// Set themes
+// https://www.amcharts.com/docs/v5/concepts/themes/
+root.setThemes([
+  am5themes_Animated.new(root)
+]);
+
+var data = {
+  value: 0,
+  children: [
+    {
+      name: "Flora",
+      children: [
+        {
+          name: "Black Tea",
+          value: 1
+        },
+        {
+          name: "Floral",
+          children: [
+            {
+              name: "Chamomile",
+              value: 1
+            },
+            {
+              name: "Rose",
+              value: 1
+            },
+            {
+              name: "Jasmine",
+              value: 1
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "Fruity",
+      children: [
+        {
+          name: "Berry",
+          children: [
+            {
+              name: "Blackberry",
+              value: 1
+            },
+            {
+              name: "Raspberry",
+              value: 1
+            },
+            {
+              name: "Blueberry",
+              value: 1
+            },
+            {
+              name: "Strawberry",
+              value: 1
+            }
+          ]
+        },
+        {
+          name: "Dried Fruit",
+          children: [
+            {
+              name: "Raisin",
+              value: 1
+            },
+            {
+              name: "Prune",
+              value: 1
+            }
+          ]
+        },
+        {
+          name: "Other Fruit",
+          children: [
+            {
+              name: "Coconut",
+              value: 1
+            },
+            {
+              name: "Cherry",
+              value: 1
+            },
+            {
+              name: "Pomegranate",
+              value: 1
+            },
+            {
+              name: "Pineapple",
+              value: 1
+            },
+            {
+              name: "Grape",
+              value: 1
+            },
+            {
+              name: "Apple",
+              value: 1
+            },
+            {
+              name: "Peach",
+              value: 1
+            },
+            {
+              name: "Pear",
+              value: 1
+            }
+          ]
+        },
+        {
+          name: "Citrus Fruit",
+          children: [
+            {
+              name: "Grapefruit",
+              value: 1
+            },
+            {
+              name: "Orange",
+              value: 1
+            },
+            {
+              name: "Lemon",
+              value: 1
+            },
+            {
+              name: "Lime",
+              value: 1
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "Sour/Fermented",
+      children: [
+        {
+          name: "Sour",
+          children: [
+            {
+              name: "Sour Aromatics",
+              value: 1
+            },
+            {
+              name: "Acetic Acid",
+              value: 1
+            },
+            {
+              name: "Butyric Acid",
+              value: 1
+            },
+            {
+              name: "Isovaleric Acid",
+              value: 1
+            },
+            {
+              name: "Citric Acid",
+              value: 1
+            },
+            {
+              name: "Malic Acid",
+              value: 1
+            }
+          ]
+        },
+        {
+          name: "Alcohol/Fremented",
+          children: [
+            {
+              name: "Winey",
+              value: 1
+            },
+            {
+              name: "Whiskey",
+              value: 1
+            },
+            {
+              name: "Fremented",
+              value: 1
+            },
+            {
+              name: "Overripe",
+              value: 1
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "Green/Vegetative",
+      children: [
+        {
+          name: "Olive Oil",
+          value: 1
+        },
+        {
+          name: "Raw",
+          value: 1
+        },
+        {
+          name: "Green/Vegetative",
+          children: [
+            {
+              name: "Under-ripe",
+              value: 1
+            },
+            {
+              name: "Peapod",
+              value: 1
+            },
+            {
+              name: "Fresh",
+              value: 1
+            },
+            {
+              name: "Dark Green",
+              value: 1
+            },
+            {
+              name: "Vegetative",
+              value: 1
+            },
+            {
+              name: "Hay-like",
+              value: 1
+            },
+            {
+              name: "Herb-like",
+              value: 1
+            }
+          ]
+        },
+        {
+          name: "Beany",
+          value: 1
+        }
+      ]
+    },
+    {
+      name: "Other",
+      children: [
+        {
+          name: "Papery/Musty",
+          children: [
+            {
+              name: "Stale",
+              value: 1
+            },
+            {
+              name: "Cardboard",
+              value: 1
+            },
+            {
+              name: "Papery",
+              value: 1
+            },
+            {
+              name: "Woody",
+              value: 1
+            },
+            {
+              name: "Moldy/Damp",
+              value: 1
+            },
+            {
+              name: "Musty/Dusty",
+              value: 1
+            },
+            {
+              name: "Musty/Earthy",
+              value: 1
+            },
+            {
+              name: "Animalic",
+              value: 1
+            },
+            {
+              name: "Meaty Brothy",
+              value: 1
+            },
+            {
+              name: "Phenolic",
+              value: 1
+            }
+          ]
+        },
+        {
+          name: "Chemical",
+          children: [
+            {
+              name: "Bitter",
+              value: 1
+            },
+            {
+              name: "Salty",
+              value: 1
+            },
+            {
+              name: "Medicinal",
+              value: 1
+            },
+            {
+              name: "Petroleum",
+              value: 1
+            },
+            {
+              name: "Skunky",
+              value: 1
+            },
+            {
+              name: "Rubber",
+              value: 1
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "Roasted",
+      children: [
+        {
+          name: "Pipe Tobacco",
+          value: 1
+        },
+        {
+          name: "Tobacco",
+          value: 1
+        },
+        {
+          name: "Burnt",
+          children: [
+            {
+              name: "Acrid",
+              value: 1
+            },
+            {
+              name: "Ashy",
+              value: 1
+            },
+            {
+              name: "Smoky",
+              value: 1
+            },
+            {
+              name: "Brown, Roast",
+              value: 1
+            }
+          ]
+        },
+        {
+          name: "Cereal",
+          children: [
+            {
+              name: "Grain",
+              value: 1
+            },
+            {
+              name: "Malt",
+              value: 1
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "Spices",
+      children: [
+        {
+          name: "Pungent",
+          value: 1
+        },
+        {
+          name: "Pepper",
+          value: 1
+        },
+        {
+          name: "Brown Spice",
+          children: [
+            {
+              name: "Anise",
+              value: 1
+            },
+            {
+              name: "Nutmeg",
+              value: 1
+            },
+            {
+              name: "Cinnamon",
+              value: 1
+            },
+            {
+              name: "Clove",
+              value: 1
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "Nutty/Cocoa",
+      children: [
+        {
+          name: "Nutty",
+          children: [
+            {
+              name: "Peanuts",
+              value: 1
+            },
+            {
+              name: "Hazelnut",
+              value: 1
+            },
+            {
+              name: "Almond",
+              value: 1
+            }
+          ]
+        },
+        {
+          name: "Cocoa",
+          children: [
+            {
+              name: "Chocolate",
+              value: 1
+            },
+            {
+              name: "Dark Chocolate",
+              value: 1
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "Sweet",
+      children: [
+        {
+          name: "Brown Sugar",
+          children: [
+            {
+              name: "Molasses",
+              value: 1
+            },
+            {
+              name: "Maple Syrup",
+              value: 1
+            },
+            {
+              name: "Caramelized",
+              value: 1
+            },
+            {
+              name: "Honey",
+              value: 1
+            }
+          ]
+        },
+        {
+          name: "Vanilla",
+          value: 1
+        },
+        {
+          name: "Vanillin",
+          value: 1
+        },
+        {
+          name: "Overall Sweet",
+          value: 1
+        },
+        {
+          name: "Sweet Aromatics",
+          value: 1
+        }
+      ]
+    }
+  ]
+};
+
+var zoomableContainer = root.container.children.push(
+  am5.ZoomableContainer.new(root, {
+    width: am5.p100,
+    height: am5.p100,
+    wheelable: true,
+    pinchZoom: true
+  })
+);
+
+var zoomTools = zoomableContainer.children.push(am5.ZoomTools.new(root, {
+  target: zoomableContainer
+}));
+
+// Create series
+// https://www.amcharts.com/docs/v5/charts/hierarchy/#Adding
+var series = zoomableContainer.contents.children.push(am5hierarchy.ForceDirected.new(root, {
+  maskContent:false, //!important with zoomable containers
+  singleBranchOnly: false,
+  downDepth: 2,
+  topDepth: 1,
+  initialDepth: 3,
+  valueField: "value",
+  categoryField: "name",
+  childDataField: "children",
+  idField: "name",
+  linkWithField: "linkWith",
+  manyBodyStrength: -10,
+  centerStrength: 0.8
+}));
+
+series.get("colors").setAll({
+  step: 2
+});
+
+series.links.template.set("strength", 0.5);
+series.labels.template.set("minScale", 0);
+
+series.data.setAll([data]);
+
+series.set("selectedDataItem", series.dataItems[0]);
+
+
+// Make stuff animate on load
+series.appear(1000, 100);
+
+}); // end am5.ready()
 
   // =======================================================
   // == PROFIT & LOSS CHART ANIMATION (D3/CSS Version) ==
